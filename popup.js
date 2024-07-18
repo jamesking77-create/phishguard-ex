afbfd64cc3c12d97bb7ed5abdceb9232dfe787e8
V-alonge <alongevictor321@gmail.com>
2024-07-18 13:53:13 +0100

    inti

--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
 document.getElementById('authButton').addEventListener('click', function() {
+    console.log('Auth button clicked');
     const iframe = document.getElementById('authFrame');
     iframe.style.display = 'block';
+    console.log('Auth iframe displayed');
 });
 
 window.addEventListener('message', function(event) {
+    console.log('Message received:', event.data);
     if (event.data === 'authenticated') {
         document.getElementById('authFrame').style.display = 'none';
         document.getElementById('status').textContent = 'Phishing detection started.';
+        console.log('User authenticated, sending start_detection message');
         chrome.runtime.sendMessage({ type: 'start_detection' });
     }
 });
--- user_auth.js
+++ user_auth.js
@@ -1,28 +1,36 @@
 document.getElementById('authForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    
     const email = document.getElementById('email').value;
     const appPassword = document.getElementById('appPassword').value;
     const feedback = document.getElementById('feedback');
 
+    console.log("Submit button clicked");  // Debugging line
+    console.log("Email:", email);  // Debugging line
+    console.log("App Password:", appPassword);  // Debugging line
 
-    fetch('http://localhost:5000/start_detection', {
+    fetch('http://127.0.0.1:5000/start_detection', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ email: email, password: appPassword })
     })
-     .then(response => response.json())
+    .then(response => response.json())
     .then(data => {
+        console.log("Response received:", data);  // Debugging line
         if (data.success) {
             feedback.textContent = 'Authentication successful. Phishing detection started.';
             feedback.style.color = '#ccffcc'; // Light green for success messages
             window.parent.postMessage('authenticated', '*');
         } else {
             feedback.textContent = `Authentication failed: ${data.error}`;
+            feedback.style.color = 'red'; // Red for error messages
         }
     })
     .catch(error => {
+        console.error("Error occurred:", error);  // Debugging line
         feedback.textContent = `Error: ${error.message}`;
+        feedback.style.color = 'red'; // Red for error messages
     });
 });
